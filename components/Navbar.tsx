bbb12ade91cf64dd12ee227006d6f1f25c54efb2
UniqueDevStorm <kirito7416@gmail.com>
2021-05-16 18:44:03 +0900

    feat: user profile in main page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,11 +20,24 @@ export default function Navbar({ data }) {
                     </a>
                 </Link>
             </div>
-            <div className='float-right text-lg p-2'>
+            <div className='float-right'>
                 {
                     data === null || data === undefined
-                    ? <a className='p-5 px-3 pt-1 text-lg' href={process.env.API_URI}>로그인</a>
-                    : <img className='rounded-full' alt={data.username} src={`https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}.png`} height={60} width={60} />
+                    ? <div className='p-5 mt-1 text-lg'>
+                        <a className='px-3 pt-1' href={process.env.API_URI}>로그인</a>
+                    </div>
+                    : <div className='sm:p-2 p-1.5'>
+                        <div className='flex'>
+                            <div className='sm:mt-5 mt-6 font-bold sm:text-xl text-sm sm:pr-4 pr-1'>
+                                {
+                                    data.username.length > 10
+                                        ? <h1>{data.username.slice(0, 10)}..#{data.discriminator}</h1>
+                                        : <h1>{data.username}#{data.discriminator}</h1>
+                                }
+                            </div>
+                            <img className='rounded-full' alt={data.username} src={`https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}.png`} height={60} width={60} />
+                        </div>
+                    </div>
                 }
             </div>
             <div className='border h-20 h-16 w-full border-gray-200' />
